b7d3521b9a54595892974a3c5dd19d971ff5cade
ChangWynn <huynhchang.one@gmail.com>
2023-06-06 17:38:07 +0100

    refactored EmptyPage

--- frontend/src/components/feed/emptyPage/EmptyPage.js
+++ frontend/src/components/feed/emptyPage/EmptyPage.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import './EmptyPage.css'
+import style from './EmptyPage.module.css';
 
 const EmptyPage = () => {
   
   return (
     <>
-      <div id='empty-page'>
+      <div className={style['empty-page']}>
         <i className="fa-solid fa-cat fa-10x"></i>
         <h3>What are you looking at?</h3>
         <h3>Say something!</h3>
