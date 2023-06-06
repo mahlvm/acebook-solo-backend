f9d2cfdc41d52367ba4d79869bcf81ce6b55b4d1
ChangWynn <huynhchang.one@gmail.com>
2023-06-06 17:23:11 +0100

    minor refactoring

--- api/controllers/tokens.js
+++ api/controllers/tokens.js
@@ -8,17 +8,13 @@ const SessionsController = {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({ email: email }).then((user) => {
-      if (!user) {
-        res.status(401).json({ message: "auth error" });
-      } else {
+    User.findOne({ email: email })
+      .then((user) => {
+        if (!user) { res.status(401).json({ message: "Email not found" }) }
+        else {
           bcrypt.compare(password, user.password, async (err, result) => {
-          if (err) {
-            res.status(401).json({message: 'Password encryption error'})
-          }
-          else if (result === false) {
-            res.status(402).json({message: 'Incorrect password'})
-          }
+          if (err) { res.status(401).json({message: 'Password encryption error'}) }
+          else if (result === false) { res.status(402).json({message: 'Incorrect password'}) }
           else {
             const token = await TokenGenerator.jsonwebtoken(user.id)
             res.status(201).json({ token: token, message: "OK" });
@@ -30,6 +26,3 @@ const SessionsController = {
 };
 
 module.exports = SessionsController;
-
-//else if (user.password !== password) {
- // res.status(402).json({ message: "auth error" });
