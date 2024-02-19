db9d9982d2ddc0847029e16dc8891a9550fb50a7
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2024-02-19 13:52:19 +0000

    Bug fixing on token tests

--- api/spec/controllers/tokens.spec.js
+++ api/spec/controllers/tokens.spec.js
@@ -1,34 +1,39 @@
 const app = require("../../app");
 const request = require("supertest");
 require("../mongodb_helper");
-const User = require('../../models/user');
+const User = require("../../models/user");
 
 describe("/tokens", () => {
-  beforeAll( () => {
-    const user = new User({ email: "test@test.com", password: "12345678" })
-    user.save()
+  beforeAll(() => {
+    //encrypted password matching how users are normally saved in the database
+    const user = new User({
+      email: "test@test.com",
+      password: "$2a$10$L.V/YQKkyu9GzRTUdIlQK.Usk0lo.CdwMYLbMZsSGxHJWtUIFFfZi",
+    });
+    user.save();
   });
 
-  afterAll( async () => {
-    await User.deleteMany({})
-  })
+  afterAll(async () => {
+    await User.deleteMany({});
+  });
 
   test("a token is returned when creds are valid", async () => {
     let response = await request(app)
       .post("/tokens")
-      .send({email: "test@test.com", password: "12345678"})
-    expect(response.status).toEqual(201)
-    expect(response.body.token).not.toEqual(undefined)
-    expect(response.body.message).toEqual("OK")
-  })
-
+      .send({ email: "test@test.com", password: "12345678" });
+    expect(response.status).toEqual(201);
+    expect(response.body.token).not.toEqual(undefined);
+    expect(response.body.message).toEqual("OK");
+  });
 
   test("a token is not returned when creds are invalid", async () => {
     let response = await request(app)
       .post("/tokens")
-      .send({email: "test@test.com", password: "1234"})
-    expect(response.status).toEqual(401)
-    expect(response.body.token).toEqual(undefined)
-    expect(response.body.message).toEqual("auth error")
-  })
-})
+      .send({ email: "test@test.com", password: "1234" });
+    expect(response.status).toEqual(402);
+    expect(response.body.token).toEqual(undefined);
+    expect(response.body.message).toEqual("Incorrect password");
+  });
+});
+
+// const saltRounds = 10;
